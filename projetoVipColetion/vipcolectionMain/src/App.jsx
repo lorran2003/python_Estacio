e97ba9117a61c8f3719b9adbce259a635c73cae6
lorran2003 <lorran23082003@gmail.com>
2024-04-04 23:59:20 -0300

    Header do site

---
+++ projetoVipColetion/vipcolectionMain/src/App.jsx
@@ -0,0 +1,14 @@
+import { Cards } from "./components/Cards";
+import { NavBar } from "./components/NavBar";
+
+export function App() {
+
+  return (
+    <main>
+      <NavBar />
+      <Cards />
+    </main>
+  )
+}
+
+
